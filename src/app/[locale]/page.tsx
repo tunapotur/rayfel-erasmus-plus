89c128f55907232ac7588073229ab4b5cd6c28f8
Ahmet Tuna POTUR <tpotur@gmail.com>
2026-05-08 11:38:58 +0300

    pages

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,114 +1,159 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { ModeToggle } from "@/components/ModeToggle";
+import Navbar from "@/components/Navbar";
 
-import { Locale, useTranslations } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
-import { use } from "react";
-
-import LocaleSwitcher from "@/components/LocaleSwitcher";
-
-export default function HomePage({ params }: PageProps<"/[locale]">) {
-  const { locale } = use(params);
-
-  // Enable static rendering
-  setRequestLocale(locale as Locale);
-
-  const t = useTranslations("HomePage");
-
+export default function HomePage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <div className="flex flex-row justify-around py-2">
-          <h1>{t("title")}</h1>
-
-          <LocaleSwitcher />
-
-          <ModeToggle></ModeToggle>
+    <div className="flex justify-center">
+      <div className="max-w-4xl ">
+        <Navbar />
+        <div className="border-2 border-amber-500 mt-12">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            quisquam, nulla doloremque vitae voluptatibus suscipit atque, optio
+            mollitia dicta est, eveniet illum magnam reprehenderit enim nemo
+            recusandae commodi repellendus consequatur. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Porro culpa harum distinctio
+            repellat eius repellendus commodi reprehenderit eligendi fugiat
+            quasi optio deserunt laborum repudiandae, ullam consequuntur
+            consectetur voluptatibus nobis voluptatum. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Provident modi officiis itaque
+            dignissimos accusantium optio deleniti, recusandae architecto quam
+            vitae id, iusto ipsum expedita praesentium eos molestias, quae
+            eveniet nisi? Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Cumque totam et excepturi fugiat iste dolore consequatur
+            inventore, quia nam sit, eaque commodi provident optio aspernatur
+            illo labore sed tempore accusantium! Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Quia debitis, quaerat, placeat error
+            provident iusto qui, reiciendis recusandae temporibus illum esse
+            ipsa nisi nobis officiis doloribus consequatur deleniti atque.
+            Quibusdam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            quisquam, nulla doloremque vitae voluptatibus suscipit atque, optio
+            mollitia dicta est, eveniet illum magnam reprehenderit enim nemo
+            recusandae commodi repellendus consequatur. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Porro culpa harum distinctio
+            repellat eius repellendus commodi reprehenderit eligendi fugiat
+            quasi optio deserunt laborum repudiandae, ullam consequuntur
+            consectetur voluptatibus nobis voluptatum. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Provident modi officiis itaque
+            dignissimos accusantium optio deleniti, recusandae architecto quam
+            vitae id, iusto ipsum expedita praesentium eos molestias, quae
+            eveniet nisi? Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Cumque totam et excepturi fugiat iste dolore consequatur
+            inventore, quia nam sit, eaque commodi provident optio aspernatur
+            illo labore sed tempore accusantium! Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Quia debitis, quaerat, placeat error
+            provident iusto qui, reiciendis recusandae temporibus illum esse
+            ipsa nisi nobis officiis doloribus consequatur deleniti atque.
+            Quibusdam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            quisquam, nulla doloremque vitae voluptatibus suscipit atque, optio
+            mollitia dicta est, eveniet illum magnam reprehenderit enim nemo
+            recusandae commodi repellendus consequatur. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Porro culpa harum distinctio
+            repellat eius repellendus commodi reprehenderit eligendi fugiat
+            quasi optio deserunt laborum repudiandae, ullam consequuntur
+            consectetur voluptatibus nobis voluptatum. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Provident modi officiis itaque
+            dignissimos accusantium optio deleniti, recusandae architecto quam
+            vitae id, iusto ipsum expedita praesentium eos molestias, quae
+            eveniet nisi? Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Cumque totam et excepturi fugiat iste dolore consequatur
+            inventore, quia nam sit, eaque commodi provident optio aspernatur
+            illo labore sed tempore accusantium! Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Quia debitis, quaerat, placeat error
+            provident iusto qui, reiciendis recusandae temporibus illum esse
+            ipsa nisi nobis officiis doloribus consequatur deleniti atque.
+            Quibusdam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            quisquam, nulla doloremque vitae voluptatibus suscipit atque, optio
+            mollitia dicta est, eveniet illum magnam reprehenderit enim nemo
+            recusandae commodi repellendus consequatur. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Porro culpa harum distinctio
+            repellat eius repellendus commodi reprehenderit eligendi fugiat
+            quasi optio deserunt laborum repudiandae, ullam consequuntur
+            consectetur voluptatibus nobis voluptatum. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Provident modi officiis itaque
+            dignissimos accusantium optio deleniti, recusandae architecto quam
+            vitae id, iusto ipsum expedita praesentium eos molestias, quae
+            eveniet nisi? Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Cumque totam et excepturi fugiat iste dolore consequatur
+            inventore, quia nam sit, eaque commodi provident optio aspernatur
+            illo labore sed tempore accusantium! Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Quia debitis, quaerat, placeat error
+            provident iusto qui, reiciendis recusandae temporibus illum esse
+            ipsa nisi nobis officiis doloribus consequatur deleniti atque.
+            Quibusdam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            quisquam, nulla doloremque vitae voluptatibus suscipit atque, optio
+            mollitia dicta est, eveniet illum magnam reprehenderit enim nemo
+            recusandae commodi repellendus consequatur. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Porro culpa harum distinctio
+            repellat eius repellendus commodi reprehenderit eligendi fugiat
+            quasi optio deserunt laborum repudiandae, ullam consequuntur
+            consectetur voluptatibus nobis voluptatum. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Provident modi officiis itaque
+            dignissimos accusantium optio deleniti, recusandae architecto quam
+            vitae id, iusto ipsum expedita praesentium eos molestias, quae
+            eveniet nisi? Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Cumque totam et excepturi fugiat iste dolore consequatur
+            inventore, quia nam sit, eaque commodi provident optio aspernatur
+            illo labore sed tempore accusantium! Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Quia debitis, quaerat, placeat error
+            provident iusto qui, reiciendis recusandae temporibus illum esse
+            ipsa nisi nobis officiis doloribus consequatur deleniti atque.
+            Quibusdam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            quisquam, nulla doloremque vitae voluptatibus suscipit atque, optio
+            mollitia dicta est, eveniet illum magnam reprehenderit enim nemo
+            recusandae commodi repellendus consequatur. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Porro culpa harum distinctio
+            repellat eius repellendus commodi reprehenderit eligendi fugiat
+            quasi optio deserunt laborum repudiandae, ullam consequuntur
+            consectetur voluptatibus nobis voluptatum. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Provident modi officiis itaque
+            dignissimos accusantium optio deleniti, recusandae architecto quam
+            vitae id, iusto ipsum expedita praesentium eos molestias, quae
+            eveniet nisi? Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Cumque totam et excepturi fugiat iste dolore consequatur
+            inventore, quia nam sit, eaque commodi provident optio aspernatur
+            illo labore sed tempore accusantium! Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Quia debitis, quaerat, placeat error
+            provident iusto qui, reiciendis recusandae temporibus illum esse
+            ipsa nisi nobis officiis doloribus consequatur deleniti atque.
+            Quibusdam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            quisquam, nulla doloremque vitae voluptatibus suscipit atque, optio
+            mollitia dicta est, eveniet illum magnam reprehenderit enim nemo
+            recusandae commodi repellendus consequatur. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Porro culpa harum distinctio
+            repellat eius repellendus commodi reprehenderit eligendi fugiat
+            quasi optio deserunt laborum repudiandae, ullam consequuntur
+            consectetur voluptatibus nobis voluptatum. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Provident modi officiis itaque
+            dignissimos accusantium optio deleniti, recusandae architecto quam
+            vitae id, iusto ipsum expedita praesentium eos molestias, quae
+            eveniet nisi? Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Cumque totam et excepturi fugiat iste dolore consequatur
+            inventore, quia nam sit, eaque commodi provident optio aspernatur
+            illo labore sed tempore accusantium! Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Quia debitis, quaerat, placeat error
+            provident iusto qui, reiciendis recusandae temporibus illum esse
+            ipsa nisi nobis officiis doloribus consequatur deleniti atque.
+            Quibusdam.
+          </p>
         </div>
-
-        <div className="flex flex-row justify-around py-2">
-          <div className="bg-sky-50 size-8 rounded"></div>
-          <div className="bg-sky-100 size-8 rounded"></div>
-          <div className="bg-sky-200 size-8 rounded"></div>
-          <div className="bg-sky-300 size-8 rounded"></div>
-          <div className="bg-sky-400 size-8 rounded"></div>
-          <div className="bg-sky-500 size-8 rounded"></div>
-          <div className="bg-sky-600 size-8 rounded"></div>
-          <div className="bg-sky-700 size-8 rounded"></div>
-          <div className="bg-sky-800 size-8 rounded"></div>
-          <div className="bg-sky-900 size-8 rounded"></div>
-          <div className="bg-sky-950 size-8 rounded"></div>
-        </div>
-
-        <div className="flex flex-row justify-around py-2">
-          <div className="bg-sky-500/10 size-8 rounded"></div>
-          <div className="bg-sky-500/20 size-8 rounded"></div>
-          <div className="bg-sky-500/30 size-8 rounded"></div>
-          <div className="bg-sky-500/40 size-8 rounded"></div>
-          <div className="bg-sky-500/50 size-8 rounded"></div>
-          <div className="bg-sky-500/60 size-8 rounded"></div>
-          <div className="bg-sky-500/70 size-8 rounded"></div>
-          <div className="bg-sky-500/80 size-8 rounded"></div>
-          <div className="bg-sky-500/90 size-8 rounded"></div>
-          <div className="bg-sky-500/100 size-8 rounded"></div>
-        </div>
-        <Card className="w-full max-w-sm">
-          <CardHeader>
-            <CardTitle>Login to your account</CardTitle>
-            <CardDescription>
-              Enter your email below to login to your account
-            </CardDescription>
-            <CardAction>
-              <Button variant="link">Sign Up</Button>
-            </CardAction>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="flex flex-col gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                  <Input id="password" type="password" required />
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex-col gap-2">
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-          </CardFooter>
-        </Card>
       </div>
     </div>
   );
