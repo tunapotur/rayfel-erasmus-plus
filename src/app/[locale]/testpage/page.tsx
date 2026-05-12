24868ef70b82ac84616c470b9f7f16165c936b7b
Ahmet Tuna POTUR <tpotur@gmail.com>
2026-05-12 12:14:50 +0300

    navbarcloude deleted

--- src/app/[locale]/testpage/page.tsx
+++ src/app/[locale]/testpage/page.tsx
@@ -1,11 +1,9 @@
-import Navbar from "@/components/NavbarCloude";
 import Hero from "@/components/Hero";
 import InfoCards from "@/components/InfoCards";
 
 export default function TestPage() {
   return (
     <div>
-      <Navbar />
       <Hero />
       <InfoCards />
     </div>
