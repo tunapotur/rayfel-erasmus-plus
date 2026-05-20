3641a63f924de5e2b49639497d84392c5e66558f
Ahmet Tuna POTUR <tpotur@gmail.com>
2026-05-20 11:35:31 +0300

    footer top border

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ const socialLinks = [
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-50 dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 mt-24">
+    <footer className="bg-gray-50 dark:bg-gray-900 mt-24">
       {/* Main Footer */}
       <div className="max-w-5xl mx-auto px-4 py-12">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
