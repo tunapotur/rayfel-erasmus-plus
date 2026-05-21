c754319fcecc0d74aaa6895f3afdae5919ef9cf1
Ahmet Tuna POTUR <tpotur@gmail.com>
2026-05-21 15:01:29 +0300

    news page

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -1,4 +1,6 @@
+import Card from "@/components/Card";
 import PageBadgeHeader from "@/components/PageBadgeHeader";
+import news from "@/sample_data/news";
 
 export default function NewsPage() {
   return (
@@ -9,6 +11,13 @@ export default function NewsPage() {
         content="Projelerimizden en son gelişmeleri ve başarı hikayelerimizi buradan takip edebilirsiniz."
         news
       />
+
+      {/* Announcements Cards List */}
+      <div className="w-full grid grid-cols-1 md:grid-cols-3 md:grid-rows-2 gap-6 px-4 py-6">
+        {news.map((item) => (
+          <Card key={item.title} card={item} />
+        ))}
+      </div>
     </div>
   );
 }
