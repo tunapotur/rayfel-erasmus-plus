032138c27834d98819fbc784b397c8d04cfa32d7
Ahmet Tuna POTUR <tpotur@gmail.com>
2026-06-03 15:21:08 +0300

    command

--- components/content/page/index.ts
+++ components/content/page/index.ts
@@ -1,3 +1,6 @@
+// Bu import dışarıda kullanılmadığı için buraya eklenmedi
+//export { default as BadgeHeader } from "./BadgeHeader";
+
 // Client Components
 export { default as PaginationOperations } from "./PaginationOperations";
 export { default as Wrapper } from "./Wrapper";
